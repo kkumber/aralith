549ee74ba657a31d452bb3a7006b34905a84acd8
Angel Bert Reyes <angelbertreyes28@gmail.com>
2025-05-27 19:58:27 +0800

    init

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -41,7 +41,7 @@
 
         @routes
         @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx", 'resources/css/app.css'])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Aralith | AI Quiz Generator</title>
+    @vite(['resources/css/app.css'])
+
+
 </head>
 <body>
-    <div class="bg-primary-dark w-20"></div>
+    <h1 class="font-headers text-4xl">Hello</h1>
     
 </body>
 </html>
