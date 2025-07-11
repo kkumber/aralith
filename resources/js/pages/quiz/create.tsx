6ed7c1307a75b15828b3598e45e8dfe73bfeaba7
kkumber <angelbertreyes28@gmail.com>
2025-07-11 11:19:08 +0800

    build: openrouter api

--- resources/js/pages/quiz/create.tsx
+++ resources/js/pages/quiz/create.tsx
@@ -35,7 +35,37 @@ const Create = () => {
         if (!lesson) return setShowModal(true);
 
         setLesson(lesson);
-    });
+
+        async function call() {
+            const res = await fetch('https://openrouter.ai/api/v1/chat/completions', {
+                method: 'POST',
+                headers: {
+                    Authorization: 'Bearer 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',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    model: 'google/gemma-3n-e2b-it:free',
+                    messages: [
+                        {
+                            role: 'user',
+                            content: `Generate 5 quiz questions from this text: ${lesson}. Answer in json format like this:
+                            {
+                            question: '',
+                            type: mcq,
+                            answer: '',
+                            }
+                            `,
+                        },
+                    ],
+                }),
+            });
+
+            const data = await res.json();
+            console.log(data);
+        }
+
+        call();
+    }, []);
 
     const handleReturnModal = () => {
         setShowModal(false);
--- scratch.ts
+++ scratch.ts
@@ -1,28 +1,29 @@
-
 async function call() {
-    const res = await fetch("https://openrouter.ai/api/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        "Authorization": "Bearer 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",
-        "HTTP-Referer": "<YOUR_SITE_URL>", // Optional. Site URL for rankings on openrouter.ai.
-        "X-Title": "<YOUR_SITE_NAME>", // Optional. Site title for rankings on openrouter.ai.
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        "model": "google/gemini-2.0-flash-exp:free",
-        "messages": [
-          {
-            "role": "user",
-            "content": [
-              {
-                "type": "text",
-                "text": "Explain Programming concepts like SOLID, DRY and more."
-              },
-            ]
-          }
-        ]
-      })
-    });
-    const data = await res.json();
-    console.log(data);
-} 
+            const res = await fetch('https://openrouter.ai/api/v1/chat/completions', {
+                method: 'POST',
+                headers: {
+                    Authorization: 'Bearer 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',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    model: 'google/gemma-3n-e2b-it:free',
+                    messages: [
+                        {
+                            role: 'user',
+                            content: `Generate 5 quiz questions from this text: ${lesson}. Answer in json format like this:
+                            {
+                            question: '',
+                            type: mcq,
+                            answer: '',
+                            }
+                            `,
+                        },
+                    ],
+                }),
+            });
+
+            const data = await res.json();
+            console.log(data);
+        }
+
+/*         call(); */
