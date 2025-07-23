ae3262c9ce14f5424255541c0283f3b97b2a9b7d
kkumber <angelbertreyes28@gmail.com>
2025-07-23 19:33:15 +0800

    save

--- resources/js/components/history/no-lesson-message.tsx
+++ resources/js/components/history/no-lesson-message.tsx
@@ -3,14 +3,16 @@ import { Button } from '../ui/button';
 
 const NoLessonMessage = () => {
     return (
-        <div className="flex flex-col items-center justify-center gap-4 text-center">
-            <FilePlus2 size={80} className="text-text-primary dark:text-dark-text-primary" />
-            <b>Ready for your first quiz?</b>
-            <p>Upload a file and create a new quiz. Uploaded lessons will show up here.</p>
-            <Button>
-                <PlusIcon />
-                New Quiz
-            </Button>
+        <div className="flex h-screen w-full items-center justify-center">
+            <div className="flex flex-col items-center justify-center gap-4 text-center">
+                <FilePlus2 size={80} className="text-text-primary dark:text-dark-text-primary" />
+                <b>Ready for your first quiz?</b>
+                <p>Upload a file and create a new quiz. Uploaded lessons will show up here.</p>
+                <Button variant={'outline'}>
+                    <PlusIcon />
+                    New Quiz
+                </Button>
+            </div>
         </div>
     );
 };
--- resources/js/pages/history.tsx
+++ resources/js/pages/history.tsx
@@ -1,8 +1,10 @@
 import NoLessonMessage from '@/components/history/no-lesson-message';
 import PreviousLessons from '@/components/history/previous-lessons';
+import { Button } from '@/components/ui/button';
 import AppLayout from '@/layouts/app-layout';
 import { BreadcrumbItem, LessonResponse, PaginatedResponse } from '@/types';
 import { Head, usePage } from '@inertiajs/react';
+import { PlusIcon } from 'lucide-react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -19,8 +21,15 @@ const History = () => {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="History" />
-            <main className="mx-auto grid max-w-screen-lg items-center justify-center p-4">
-                <h1 className="font-medium">Previous Lessons</h1>
+            <main className="mx-auto flex h-full max-w-screen-lg flex-col p-4">
+                <div className="flex items-center justify-between">
+                    <h1 className="self-start text-start font-medium">Previous Lessons</h1>
+                    <Button className="flex items-center justify-center">
+                        <PlusIcon />
+                        New Quiz
+                    </Button>
+                </div>
+                <input type="text" name="lesson_search" id="lessonSearch" placeholder="Search your lessons..." className="border-2" />
                 {lessons && lessons.data.length ? <PreviousLessons lessons={lessons.data} /> : <NoLessonMessage />}
             </main>
         </AppLayout>
