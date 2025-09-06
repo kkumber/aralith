792e015ea8035268e8db468149e0f260cb8fa529
Angel Bert Reyes <angelbertreyes28@gmail.com>
2025-09-06 20:13:29 +0800

    feat: added more ai models

--- config/ai.php
+++ config/ai.php
@@ -14,10 +14,11 @@ return [
         'api_key' => env('GROQ_API_KEY'),
         'models' => [
             'kimi-k2' => 'moonshotai/kimi-k2-instruct', // chose this because of high rpm and supports structured outputs
-            'meta-llama' => 'meta-llama/llama-guard-4-12b',
             'qwen3' => 'qwen/qwen3-32b',
             'openai' => 'openai/gpt-oss-20b',
-            'gemma2' => 'gemma2-9b-it'
+            'gemma2' => 'gemma2-9b-it',
+            'llama-instant' => 'llama-3.1-8b-instant',
+            'llama-versatile' => 'llama-3.3-70b-versatile',
         ]
     ],
     'prompts' => [
