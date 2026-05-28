b30b9eee1a680522e805fa5ae3a0fbb8f841997e
Angel Bert Reyes <areyes@insightqgroup.com>
2026-05-28 22:34:32 +0800

    dev: docker files and compose

--- docker/Dockerfile.dev.php
+++ docker/Dockerfile.dev.php
@@ -18,14 +18,17 @@ RUN apk add --no-cache \
     zip \
     unzip \
     && docker-php-ext-configure gd --with-freetype --with-jpeg --with-webp \
-    && docker-php-ext-install pdo pdo_pgsql mbstring exif pcntl bcmath gd
+    && docker-php-ext-install pdo pdo_pgsql mbstring exif pcntl bcmath gd zip
 
 # Get Composer
 COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
 
+COPY composer.json composer.lock ./
+
+RUN composer install --no-interaction --no-plugins --no-scripts
+
 COPY . .
 
-RUN composer install 
 
 RUN echo "upload_max_file_size=10M" > /usr/local/etc/php/conf.d/upload.ini && \
     echo "post_max_size=10M" >> /usr/local/etc/php/conf.d/upload.ini && \
@@ -34,4 +37,4 @@ RUN echo "upload_max_file_size=10M" > /usr/local/etc/php/conf.d/upload.ini && \
 
 EXPOSE 8000
 
-CMD [ "php", "artisan", "serve" ]
+CMD php artisan package:discover --ansi && php artisan serve --host=0.0.0.0
