569ad0bf6bc623ad056ccb388a2224d18bc81398
Angel Reyes <angelbertreyes28@gmail.com>
2026-05-31 12:57:51 +0800

    prod: fix db transaction fail on quiz creation for neon pooling

--- app/Services/LessonQuizService.php
+++ app/Services/LessonQuizService.php
@@ -15,20 +15,21 @@ class LessonQuizService
     public function createLessonSummaryFlashcardQuiz(array $lessonData, array $quizData, array $questionsData, array $flashcardsData, User $user)
     {
         try {
+
+            // Convert each element from questionsData to associative array.
+            $questionArray = [];
+            foreach ($questionsData as $key => $value) {
+                $questionArray[$key] = (array) $value;
+            };
+
+            $flashcardsArray = [];
+            foreach ($flashcardsData as $key => $value) {
+                $flashcardsArray[$key] = (array) $value;
+            };
+
             return DB::transaction(function () use ($lessonData, $quizData, $questionsData, $flashcardsData, $user) {
                 $lesson = $user->lessons()->create($lessonData);
                 $quiz = $user->quizzes()->create(['lessons_id' => $lesson->id, 'title' => $quizData['title'], 'config' => $quizData['config']]);
-
-                // Convert each element from questionsData to associative array.
-                $questionArray = [];
-                foreach ($questionsData as $key => $value) {
-                    $questionArray[$key] = (array) $value;
-                };
-
-                $flashcardsArray = [];
-                foreach ($flashcardsData as $key => $value) {
-                    $flashcardsArray[$key] = (array) $value;
-                }
 
                 $flashcards = $lesson->flashcard()->createMany($flashcardsArray);
 
@@ -52,14 +53,14 @@ class LessonQuizService
 
     public function bulkDestroyLessonQuiz(User $user, array $lessonIds)
     {
-        return DB::transaction(function () use ($user, $lessonIds) {
-            $lessons = $user->lessons()->whereIn('id', $lessonIds)->get();
+        $lessons = $user->lessons()->whereIn('id', $lessonIds)->get();
 
-            foreach ($lessons as $lesson) {
-                if (!$user->can('delete', $lesson)) {
-                    throw new AuthorizationException('You do not have permission to delete this lesson.');
-                };
+        foreach ($lessons as $lesson) {
+            if (!$user->can('delete', $lesson)) {
+                throw new AuthorizationException('You do not have permission to delete this lesson.');
             };
+        };
+        return DB::transaction(function () use ($user, $lessonIds) {
 
             $deleteCount = $user->lessons()->whereIn('id', $lessonIds)->delete();
             return $deleteCount;
